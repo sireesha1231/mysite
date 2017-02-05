72efd11a8ee6679b67aff9010648e43fe161ce75
Sireesha Basamsetty <sireesha1994@gmail.com>
2017-02-05 04:24:33 -0600

    change

--- interactive-resume.js
+++ interactive-resume.js
@@ -7,16 +7,3 @@ $('.overlay-menu li a').click(function() {
 	$('#toggle').toggleClass('active');
 	$('#overlay').toggleClass('open');
 });
-
-  $("#project1").animatedModal({
-  	animatedIn: 'fadeIn',
-  	animatedOut: 'fadeOut',
-  	color: 'rgba(0,0,0,1)'
-  });
-
-  $("#project2").animatedModal({
-  	modalTarget: 'animatedModal2',
-  	animatedIn: 'fadeIn',
-  	animatedOut: 'fadeOut',
-  	color: 'rgba(0,0,0,1)'
-  });
